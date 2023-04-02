5769f43ae5176a728b1b1fc37c3f3a13d5cbf447
arun-kumar19 <arunmaurya1177@gmail.com>
2023-04-02 11:26:25 +0530

    dom project

---
+++ dom.js
@@ -0,0 +1,25 @@
+const title=document.title;
+document.title="New Title"
+//console.log(title)
+const heading=document.getElementsByTagName("h1")
+//heading[0].innerText="New Heading1"
+const header=document.getElementById("main-header");
+header.className+=" border border-dark";
+//console.log(document.getElementsByTagName("h1").value)
+document.getElementById("input").value="Enter item name"
+//console.log(document.getElementById("input").value)
+var items=document.querySelector(".list-group");
+//console.log("first child element="+items.firstElementChild.textContent)
+
+for(var i=0; i<items.childElementCount;i++){
+
+    if(i % 2==0)
+        items.children[i].setAttribute("style","color:red");
+    else
+        items.children[i].setAttribute("style","color:blue");
+}
+
+console.log(document.querySelectorAll("input")[1].name)
+document.getElementById("title").style.fontWeight="bold"
+document.getElementById("title").style.color="green"
+document.getElementsByTagName("span")[0].style.display="none";
