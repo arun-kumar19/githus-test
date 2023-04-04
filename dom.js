0ebfe028bcfc96fed15e3550a4d141538579b7a0
arun-kumar19 <arunmaurya1177@gmail.com>
2023-04-04 08:04:16 +0530

    task5

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-const title=document.title;
+/* const title=document.title;
 document.title="New Title"
 //console.log(title)
 const heading=document.getElementsByTagName("h1")
@@ -19,16 +19,27 @@ for(var i=0; i<items.childElementCount;i++){
         items.children[i].setAttribute("style","color:red");
     else
         items.children[i].setAttribute("style","color:blue");
-}
-
+} */
+/* 
 console.log(document.querySelectorAll("input")[1].name)
 document.getElementById("title").style.fontWeight="bold"
 document.getElementById("title").style.color="green"
 document.getElementsByTagName("span")[0].style.display="none";
 
 console.log(document.getElementsByName("input")[0].name);
-console.log(document.getElementById("header-title").innerText);
-const list=document.getElementsByClassName("list-group-item")
-list[2].style.backgroundColor='green';
+console.log(document.getElementById("header-title").innerText); */
+
+/* const list=document.getElementsByClassName("list-group-item")
+
+for(var i=0;i<list.length;i++){
+    list[i].style.backgroundColor="#ccc"
+} */
+
+const li=document.getElementsByTagName("li")
+
+
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor="#ccc"
+}
 
 
