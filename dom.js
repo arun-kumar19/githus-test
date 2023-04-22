4755e1e8d3e26b485d9c593979f4139229089ea0
arun-kumar19 <arunmaurya1177@gmail.com>
2023-04-22 09:36:30 +0530

    Edit Button

--- dom.js
+++ dom.js
@@ -140,7 +140,9 @@ function removeItem(e){
     if(e.target.classList.contains('delete')){
         if(confirm('Are you sure?')){
         var li=e.target.parentElement;
+		console.log(li)
      itemlist.removeChild(li)        
     }
 }
 }
+
